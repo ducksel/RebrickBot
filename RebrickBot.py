06597f1a3dcea1565dfc7fab2e9b253ad068e417
ducksel <stas.seleznev@gmail.com>
2025-04-09 21:23:42 +0200

    v.0.0.2

--- RebrickBot.py
+++ RebrickBot.py
@@ -3,8 +3,16 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler
 
 # Функция-обработчик для команды /start
-async def start(update: Update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	user = update.effective_user
+	print(f"/start от {user.username or user.id}")
 	await update.message.reply_text("Привет, мир! Я бот готов.")  # бот отвечает сообщением
+
+# Функция-обработчик для текстовой команды от пользователя
+async def handle_text(update: Update, context):
+	text = update.message.text
+	print(f"Пользователь прислал: {text}")
+	
 
 app = ApplicationBuilder().token(os.environ["BOT_TOKEN"]).build()       # создание приложения бота с токеном
 app.add_handler(CommandHandler("start", start))             # привязываем команду /start к функции
